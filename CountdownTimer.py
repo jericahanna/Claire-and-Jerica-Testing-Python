ac1e1434a5cf3dd4db51da90e86f46d212b50bdd
Jerica <jericahanna04@gmail.com>
2025-09-07 14:28:42 -0500

    Fixing the folders?

---
+++ CountdownTimer.py
@@ -0,0 +1,18 @@
+# countdown timer
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer) # end="\r (This gets rid of the numbers)"
+        time.sleep(1)
+        t -= 1
+
+    print("Time completed!")
+
+t = input('Enter the time in seconds: ')
+
+countdown(int(t))
+
+print(t)
---
+++ GuessTheNumberGame.py
@@ -0,0 +1,38 @@
+# Python number guessing game
+import random
+from multiprocessing.connection import answer_challenge
+
+lowest_num = 1
+highest_num = 100
+answer = random.randint(lowest_num, highest_num)
+guesses = 0
+is_running = True
+
+print("Welcome to Jerica Hanna's Number guessing game")
+print(f"Select a number between {lowest_num} and {highest_num}")
+
+while is_running:
+
+    guess = input("Enter your # guess: ")
+
+    if guess.isdigit():
+        guess = int(guess)
+        guesses += 1
+
+        if guess < lowest_num or guess > highest_num:
+            print("That number is out of range")
+            print(f"Select a number between {lowest_num} and {highest_num}")
+        elif guess < answer:
+            print("Too low! Try again!")
+        elif guess > answer:
+            print("Too High! Try again!")
+        else:
+            print(f"Correct! The nswer was {answer}")
+            print(f"Number of guesses: {guesses}")
+            is_running = False
+    else:
+        print("Invalid guess")
+        print(f"Select a number between {lowest_num} and {highest_num}")
+
+
+
